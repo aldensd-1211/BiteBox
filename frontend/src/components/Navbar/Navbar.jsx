bbd9e2bcf9f4c7a56fd5840ea9d9c597866cc2a0
aldensd-1211 <aldendcunha1211@gmail.com>
2024-08-22 12:27:18 +0530

    Create Signup/Login component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
@@ -44,7 +44,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>Sign up</button>
+        <button onClick={() => setShowLogin(true)}>Sign up</button>
       </div>
     </div>
   );
